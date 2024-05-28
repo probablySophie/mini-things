5257883480f213ce08c86a20b17945f91c6fad63
Sophie <>
2024-05-28 12:48:12 +0800

    Moving the game out to its own scene file

--- TUI/mini_wordle/src/main.rs
+++ TUI/mini_wordle/src/main.rs
@@ -1,6 +1,8 @@
+use scenes::game::MainGame;
 use TUI::{scene::{TerminalScene, UpdateType}, Window};
 
-
+mod scenes;
+mod board;
 
 fn main ()
 {
@@ -18,59 +20,3 @@ fn main ()
 	window.destroy();
 }
 
-struct MainGame
-{
-	scene_values: TUI::scene::SceneValues,
-}
-
-impl TerminalScene for MainGame
-{
-    fn break_key(&mut self) -> u8 {
-        self.scene_values.break_key()
-    }
-
-    fn set_break_key(&mut self, key: u8) {
-        self.scene_values.set_break_key(key);
-    }
-	//
-}
-
-impl mini_core::Scene for MainGame
-{
-	fn logic (&mut self)
-	{
-		TUI::print::string("logic".to_owned(), 1, 4);
-	}
-
-	fn draw (&mut self)
-	{
-		TUI::print::string("draw".to_owned(), 10, 10);
-	}
-
-	fn key_pressed(&mut self, key: u8) {
-        // TODO:
-    }
-
-
-	fn should_exit (&mut self) -> bool
-	{
-		self.scene_values.should_exit()
-	}
-
-	fn set_exit (&mut self, new_value: bool)
-	{
-		self.scene_values.set_exit(new_value);
-	}
-}
-
-
-impl Default for MainGame
-{
-	fn default() -> Self
-	{
-		Self
-		{
-			scene_values: TUI::scene::SceneValues::default(),
-		}
-	}
-}
