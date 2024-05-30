2bf7d1e07d8b97cf64b4282cb0e4a43b729ee35c
Sophie <>
2024-05-30 13:55:06 +0800

    changes, some important, some REALLY not

--- TUI/test_reflex/src/main.rs
+++ TUI/test_reflex/src/main.rs
@@ -1,32 +1,83 @@
-use crossterm::{event::{read, KeyCode, MouseEventKind}, execute, terminal};
-use std::io::{self, stdout};
+use crossterm::{event::{read, KeyCode, KeyEvent, MouseButton, MouseEvent}, execute, terminal};
+use std::io::stdout;
+
+
+
 
 struct Scene
 {
+	exit_key: KeyCode,
 	should_exit: bool,
+}
+trait SceneFunctions
+{
+	fn logic();
+	fn draw();
 }
 
 impl Scene
 {
+	// https://docs.rs/crossterm/latest/crossterm/event/enum.KeyCode.html
 	fn key_pressed (&mut self, keycode: KeyCode)
 	{
 		match keycode
 		{
-			KeyCode::Char('q') => {self.should_exit = true},
+			KeyCode::Char('q') => { },
 			KeyCode::Char('w') => println!("You pressed W!"),
 			_ => {},
+		}	
+	}
+
+	fn mouse_pressed (&mut self, x: u16, y: u16, button: MouseButton)
+	{
+		println!("{} {} {:?}", x, y, button);
+	}
+
+	// TODO: have update types: on_event & constant
+	fn run (&mut self)
+	{
+		// Our main loop
+		loop
+		{
+			let event = read().unwrap();
+		
+			self.match_event(event);
+
+			if self.should_exit
+			{
+				break;
+			}
 		}
 	}
 
-	fn run (&mut self)
+	fn match_event (&mut self, event: crossterm::event::Event)
 	{
-		loop
-		{
-			match read().unwrap()
+		match event
 			{
-				crossterm::event::Event::Key(keyevent) => { self.key_pressed(keyevent.code) },
+				// https://docs.rs/crossterm/latest/crossterm/event/struct.KeyEvent.html
+				// https://docs.rs/crossterm/latest/crossterm/event/enum.KeyEventKind.html
+				crossterm::event::Event::Key( KeyEvent {
+					kind: crossterm::event::KeyEventKind::Press,
+					code,
+					.. }
+				) => {
+					if code == self.exit_key 
+					{
+						self.should_exit = true;
+						return
+					}
+					self.key_pressed(code);
+				},
 
-				crossterm::event::Event::Mouse(kind: MouseEventKind::Down, mouseevent) => {},
+				// https://docs.rs/crossterm/latest/crossterm/event/struct.MouseEvent.html
+				crossterm::event::Event::Mouse( MouseEvent { 
+					kind: crossterm::event::MouseEventKind::Down(button),
+					row,
+					column,
+					.. } 
+				) => {  
+					self.mouse_pressed(column, row, button);
+				},
 
 				//crossterm::event::Event::FocusGained => todo!(),
 				//crossterm::event::Event::FocusLost => todo!(),
@@ -35,12 +86,6 @@ impl Scene
 			
 				_ => {},
 			}
-
-			if self.should_exit
-			{
-				break;
-			}
-		}
 	}
 }
 
@@ -50,12 +95,17 @@ fn main()
 	terminal::enable_raw_mode()
 			.expect("Could not enable raw mode");
 
-	execute!(stdout(),	
+	execute!(stdout(),
 		crossterm::event::EnableMouseCapture,
 	).expect("");
 
-			
+	
+	let mut scene = Scene {
+		should_exit: false,
+		exit_key: KeyCode::Esc,
+	};
 
+	scene.run();
 	
 	execute!(stdout(),
 		crossterm::event::DisableMouseCapture,
